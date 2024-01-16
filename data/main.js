13115577bea77177759ab3fbba089b8c9cec437c
DESKTOP-L3GF6BJ\Luca <lucavolpato00@gmail.com>
2024-01-16 23:02:02 +0100

    [ADD][WIP] Initial draft of the home page

---
+++ data/main.js
@@ -0,0 +1,17 @@
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleMenu() {
+    var x = document.getElementById("sideNavbar");
+    if (x.className.indexOf("show") == -1) {
+        x.className += " show";
+    } else {
+        x.className = x.className.replace(" show", "");
+    }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById('ticketModal');
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
